4e915a6acd4696061067cf5d757d0490e8a0c92b
trevor millner <trevormillner@yahoo.com>
2016-09-25 18:31:18 +0800

    task - scrollable draw + match logic

--- MyApplication/app/src/main/java/com/flyhigh/tmillner/jigsawpicture/MainScreen.java
+++ MyApplication/app/src/main/java/com/flyhigh/tmillner/jigsawpicture/MainScreen.java
@@ -2,6 +2,7 @@ package com.flyhigh.tmillner.jigsawpicture;
 
 import android.content.ClipData;
 import android.content.ClipDescription;
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Point;
@@ -9,113 +10,184 @@ import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.DragEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class MainScreen extends AppCompatActivity {
 
-    private static final String JIGSAW_TAG = "jigsaw_tag1";
-    private static final String TILE_TAG = "tile_tag1";
     public static final String TAG = "MAINSCREEN";
+    private static Integer columns = 3;
+    private static Integer rows = 3;
+    private static final String CHUNK_PREFIX = "img";
 
+    private View.OnLongClickListener mLongClickListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            Log.i(TAG, "INSIDE");
+            ClipData.Item item = new ClipData.Item((CharSequence) v.getTag());
+            ClipData dragData = new ClipData((CharSequence) v.getTag(),
+                    new String[]{ClipDescription.MIMETYPE_TEXT_PLAIN}, item);
+            View.DragShadowBuilder shadow = new MyDragShadowBuilder(v);
+            v.startDrag(dragData,  // the data to be dragged
+                    shadow,  // the drag shadow builder
+                    v,      // no need to use local data
+                    0          // flags (not currently used, set to 0)
+            );
+            return true;
+        }
+    };
+
+    private View.OnDragListener mDragListener = new View.OnDragListener() {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DROP:
+                    Log.i(TAG, "IN DROP");
+
+                    ImageView dragged = (ImageView) event.getLocalState();
+
+                    Log.i(TAG, " v is " + v.toString() + v.getTag()); // tile tag
+                    Log.i(TAG, " view is " + v.toString() + dragged.getTag()); // jigsaw tag
+
+                    if (v.getTag() == dragged.getTag()) {
+                        ((LinearLayout)dragged.getParent()).removeView(dragged);
+                        ((ImageView)v).setImageDrawable(dragged.getDrawable());
+                    }
+                    break;
+            }
+            return true;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
-        View.OnLongClickListener mLongClickListener = new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                Log.i(TAG, "INSIDE");
-                ClipData.Item item = new ClipData.Item((CharSequence) v.getTag());
-                ClipData dragData = new ClipData((CharSequence) v.getTag(),
-                        new String[]{ClipDescription.MIMETYPE_TEXT_PLAIN}, item);
-                View.DragShadowBuilder shadow = new MyDragShadowBuilder(v);
-                v.startDrag(dragData,  // the data to be dragged
-                        shadow,  // the drag shadow builder
-                        v,      // no need to use local data
-                        0          // flags (not currently used, set to 0)
-                );
-                return true;
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        int[] drawables = new int[rows*columns];
+
+        // Run the function to break up a picture save the rows/columns
+        int chunk =0;
+        for (int col =0; col < columns; col++){
+            for (int row=0; row < rows; row++) {
+                drawables[chunk] = chunk;
+                GridLayout grid = (GridLayout) findViewById(R.id.gridLayout);
+
+                // Set tile
+                ImageView tile = new ImageView(getApplicationContext());
+                tile.setImageResource(R.drawable.tile);
+                //tile.setImageResource(getResources().getIdentifier(
+                //        CHUNK_PREFIX + chunk, "drawable", getPackageName()));
+                ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(150, 150);
+                tile.setLayoutParams(lp);
+                Log.i(TAG, "chunk is " + chunk);
+                tile.setTag(Integer.toString(chunk));
+                //tile.getLayoutParams().height = 500;
+                //tile.getLayoutParams().width = 500;
+                tile.setOnDragListener(mDragListener);
+                grid.addView(tile);
+                // Set jigsaw
+
+                LinearLayout drawer = (LinearLayout) findViewById(R.id.drawer);
+                Log.i(TAG, drawer.toString());
+                Log.i(TAG, drawer.getLayoutParams().toString());
+                ImageView jigsaw = new ImageView(getApplicationContext());
+                LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(150, 150);
+                jigsaw.setImageResource(getResources().getIdentifier(
+                        CHUNK_PREFIX + chunk, "drawable", getPackageName()));
+
+                jigsaw.setLayoutParams(lp2);
+                jigsaw.setTag(Integer.toString(chunk));
+                jigsaw.setOnLongClickListener(mLongClickListener);
+
+                drawer.addView(jigsaw);
+
+                chunk++;
             }
-        };
+        }
 
-        View.OnDragListener mDragListener = new View.OnDragListener() {
-            @Override
-            public boolean onDrag(View v, DragEvent event) {
-                switch (event.getAction()) {
-                    case DragEvent.ACTION_DROP:
-                        Log.i(TAG, "IN DROP");
+        /*
+        for(int i=0; i<drawables.length; i++) {
 
-                        ImageView dragged = (ImageView) event.getLocalState();
-
-                        Log.i(TAG, " v is " + v.toString() + v.getTag()); // tile tag
-                        Log.i(TAG, " view is " + v.toString() + dragged.getTag()); // jigsaw tag
-
-                        if (v.getTag() == dragged.getTag()) {
-                            ((LinearLayout)dragged.getParent()).removeView(dragged);
-                            ((ImageView)v).setImageDrawable(dragged.getDrawable());
-                        }
-                        break;
-                }
-                return true;
-            }
-        };
+            Drawable jigsaw = ResourcesCompat.getDrawable(
+                    getResources(),
+                    getResources().getIdentifier(
+                            "img" + i + ".jpg", "drawable", getPackageName()),
+                    null);
+        }
 
         final ImageView jigsaw = (ImageView) findViewById(R.id.jigsaw_piece1);
-        jigsaw.setTag("1");
+        jigsaw.setTag("0");
         jigsaw.setOnLongClickListener(mLongClickListener);
 
+        final ImageView jigsaw2 = (ImageView) findViewById(R.id.jigsaw_piece2);
+        jigsaw2.setTag("1");
+        jigsaw2.setOnLongClickListener(mLongClickListener);
+
+        */
+
+        /*
         final ImageView drop_tile = (ImageView) findViewById(R.id.tile1);
         drop_tile.setTag("1");
         drop_tile.setOnDragListener(mDragListener);
+
+        final ImageView drop_tile2 = (ImageView) findViewById(R.id.tile2);
+        drop_tile.setTag("2");
+        drop_tile2.setOnDragListener(mDragListener);
+        */
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_mainscreen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch(menuItem.getItemId()) {
+            //case (R.id.one):
+                //startActivity(new Intent(this, SettingsActivity.class));
+                //return true;
+            default:
+                return super.onOptionsItemSelected(menuItem);
+        }
     }
 
     private static class MyDragShadowBuilder extends View.DragShadowBuilder {
 
         private static Drawable shadow;
-
         public MyDragShadowBuilder(View v) {
-
-            // Stores the View parameter passed to myDragShadowBuilder.
             super(v);
-            // Creates a draggable image that will fill the Canvas provided by the system.
             shadow = new ColorDrawable(Color.LTGRAY);
         }
 
-        // Defines a callback that sends the drag shadow dimensions and touch point back to the
-        // system.
         @Override
         public void onProvideShadowMetrics (Point size, Point touch) {
-            // Defines local variables
             int width, height;
 
             width = getView().getWidth() / 2;
             height = getView().getHeight() / 2;
 
-            // The drag shadow is a ColorDrawable. This sets its dimensions to be the same as the
-            // Canvas that the system will provide. As a result, the drag shadow will fill the
-            // Canvas.
             shadow.setBounds(0, 0, width, height);
 
-            // Sets the size parameter's width and height values. These get back to the system
-            // through the size parameter.
             size.set(width, height);
-
-            // Sets the touch point's position to be in the middle of the drag shadow
             touch.set(width / 2, height / 2);
         }
 
-        // Defines a callback that draws the drag shadow in a Canvas that the system constructs
-        // from the dimensions passed in onProvideShadowMetrics().
         @Override
         public void onDrawShadow(Canvas canvas) {
-
-            // Draws the ColorDrawable in the Canvas passed in from the system.
             shadow.draw(canvas);
         }
     }
